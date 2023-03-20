8357ae932e93d3129437b385721e11113bbadc9c
Gudivni <tamerlan.almazbekov@gmail.com>
2023-03-20 21:40:48 +0600

    add 2

--- movie/models.py
+++ movie/models.py
@@ -3,7 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
+    director = models.ForeignKey(Director, on_delete=models.PROTECT, null=True)
+    genres = models.ManyToManyField(Genre, blank=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     rating = models.FloatField()
@@ -14,3 +31,24 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.name
+
+    @property
+    def filtered_review_list(self):
+        return self.review_list.filter(stars__gt=3)
+
+    @property
+    def genres_list(self):
+        return [genre.name for genre in self.genres.all()]
+
+
+STAR_CHOICES = ((iterator_, '* ' * iterator_) for iterator_ in range(1, 6))
+
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='review_list')
+    text = models.TextField()
+    stars = models.IntegerField(default=5, choices=STAR_CHOICES)
+
+    def __str__(self):
+        return self.text
--- movie/serializers.py
+++ movie/serializers.py
@@ -1,9 +1,37 @@
 from rest_framework import serializers
-from movie.models import Movie
+from movie.models import Movie, Director, Genre, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id text stars'.split()
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = 'id name age'.split()
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = 'id name'.split()
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer()
+    genres = GenreSerializer(many=True)
+    director_name = serializers.SerializerMethodField()
+    filtered_review_list = ReviewSerializer(many=True)
+
     class Meta:
         model = Movie
-        fields = 'id name rating duration'.split()
-        # exclude = 'updated created'.split()
+        fields = 'filtered_review_list director director_name genres genres_list id name rating duration'.split()
+
+    def get_director_name(self, movie):
+        try:
+            return movie.director.name
+        except:
+            return None
